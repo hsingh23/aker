0f260c437c72c1943fb1cba254edd9ec4bf2f3b0
Harsh Singh <hisingh1@gmail.com>
2014-05-10 19:14:34 -0500

    adds images

--- app.py
+++ app.py
@@ -19,12 +19,12 @@ def default():
 
 
 @app.route("/<query>")
-def hello(query):
+def web(query):
     query = query.encode('ascii', 'ignore').lower().strip()
     if not query:
         return jsonify(["http://upload.wikimedia.org/wikipedia/commons/3/35/SMirC-what.svg"])
     try:
-        cached = cache.get(cache="links", key=query).value
+        cached = cache.get(cache="web", key=query).value
         if "callback" in request.args:
             return "{callback}({data});".format(callback=request.args.get("callback"), data=cached)
         else:
@@ -32,7 +32,24 @@ def hello(query):
     except r.HTTPError:
         data = s.get("http://search.aol.com/aol/search?q={}".format(query)).text
         links = [link.attrib["href"] for link in html.fromstring(data).find_rel_links("f:url")]
-        cache.put(cache="links", key=query, value=str(links))
+        cache.put(cache="web", key=query, value=str(links))
+        return jsonify(links)
+
+@app.route("/images/<query>")
+def images(query):
+    query = query.encode('ascii', 'ignore').lower().strip()
+    if not query:
+        return jsonify(["http://upload.wikimedia.org/wikipedia/commons/3/35/SMirC-what.svg"])
+    try:
+        cached = cache.get(cache="images", key=query).value
+        if "callback" in request.args:
+            return "{callback}({data});".format(callback=request.args.get("callback"), data=cached)
+        else:
+            return cached
+    except r.HTTPError:
+        data = s.get("http://search.aol.com/aol/image?q={}".format(query)).text
+        links = [link.attrib["href"] for link in html.fromstring(data).find_rel_links("f:url")]
+        cache.put(cache="images", key=query, value=str(links))
         return jsonify(links)
 
 if __name__ == "__main__":
